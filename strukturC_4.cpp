0f4831530011336a3363b0302de50d8961411053
kevinnadr <kevin19305.ib@gmail.com>
2024-04-02 19:53:15 +0700

    gunakan for

--- strukturC_4.cpp
+++ strukturC_4.cpp
@@ -15,4 +15,13 @@ struct mahasiswa {
 
 int main(){
     mahasiswa mhs[3];
-    
+    for(int i=0;i<3;i++) {
+        cout <<"Data ke-"<<(i+1)<<":" <<endl;
+        cout << "nomor mahasiswa: ";
+        getline(cin,mhs[i].nim);
+        cout << "nama mahasiswa : ";
+        getline(cin,mhs[i].nama);
+        
+
+
+}
