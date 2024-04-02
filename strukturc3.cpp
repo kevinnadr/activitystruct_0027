c227d9b8c8ec34f452ede0ac8a0d8c9bac65602e
kevinnadr <kevin19305.ib@gmail.com>
2024-04-02 19:23:48 +0700

    gunakan ignore pada alamat desa

--- strukturc3.cpp
+++ strukturc3.cpp
@@ -20,3 +20,12 @@ int main(){
     cout << "nama mahasiswa : ";
     getline(cin,mhs.nama);
     
+
+    cout << "alamat mahasiswa : "<<endl;
+    cout << "\t nama desa : ";
+    cin >> mhs.alamat.desa;
+    cin.ignore(1,'\n');
+    
+   
+
+}
