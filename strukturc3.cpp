ab5ad410c2293b33577f472149062168758967cc
kevinnadr <kevin19305.ib@gmail.com>
2024-04-02 19:21:41 +0700

    gunakan getline 1

--- strukturc3.cpp
+++ strukturc3.cpp
@@ -17,27 +17,7 @@ int main(){
     mahasiswa mhs;
     cout << "nomor mahasiswa: ";
     getline(cin,mhs.nim);
-    cout << "nama mahasiswa : ";
-    getline(cin,mhs.nama);
     
-
-    cout << "alamat mahasiswa : "<<endl;
-    cout << "\t nama desa : ";
-    cin >> mhs.alamat.desa;
-    cin.ignore(1,'\n');
-    cout << "\t nama kota : ";
-    cin >> mhs.alamat.kota;
-    cin.ignore(1,'\n');
-
-    cout << "umur mahasiswa : ";
-    cin >> mhs.umur;
-
-    cout << endl;
-    cout <<"\n nim : "<<mhs.nim;
-    cout <<"\n nama : "<<mhs.nama;
-    cout <<"\n alamat : ";
-    cout <<"\n \t desa : "<< mhs.alamat.desa;
-    cout <<"\n \t kota : "<< mhs.alamat.kota;
    
 
 }
