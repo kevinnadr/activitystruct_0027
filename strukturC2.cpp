97f509c03688952dda953c5b0ae7312b1c45f82b
kevinnadr <kevin19305.ib@gmail.com>
2024-04-02 18:58:36 +0700

    masukan detail alamat kota

--- strukturC2.cpp
+++ strukturC2.cpp
@@ -23,5 +23,7 @@ int main(){
     cout << "alamat mahasiswa : "<<endl;
     cout << "\t nama desa : ";
     cin >> mhs.alamat.desa;
-   
+    cout << "\t nama kota : ";
+    cin >> mhs.alamat.kota;
+
 }
