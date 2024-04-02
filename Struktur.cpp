30a3a3f7fef5f58f2c9228f7d51e9c1055527ee9
kevinnadr <kevin19305.ib@gmail.com>
2024-04-02 18:32:46 +0700

    int main

--- Struktur.cpp
+++ Struktur.cpp
@@ -10,4 +10,19 @@ struct mahasiswa {
 
 int main(){
     mahasiswa mhs;
+    cout << "nomor mahasiswa: ";
+    cin >> mhs.nim;
+    cout << "nama mahasiswa : ";
+    cin >> mhs.nama;
+    cout << "alamat mahasiswa : ";
+    cin >> mhs.alamat;
+    cout << "umur mahasiswa : ";
+    cin >> mhs.umur;
+
+    cout << endl;
+    cout <<"\n nim : "<<mhs.nim;
+    cout <<"\n nama : "<<mhs.nama;
+    cout <<"\n alamat : "<<mhs.alamat;
+    cout <<"\n umur : "<<mhs.umur;
+
 }
